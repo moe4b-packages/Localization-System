a73e436506819c052ad0c8574cf5256654857361
Moe-Baker <moe4baker@gmail.com>
2021-12-04 10:34:46 +0200

    Work

--- Localization.cs
+++ Localization.cs
@@ -19,12 +19,91 @@ namespace MB.LocalizationSystem
     [LoadOrder(Runtime.Defaults.LoadOrder.LocalizationSystem)]
     public partial class Localization : ScriptableManager<Localization>
     {
+        //Instance
+        #region
+        [SerializeField]
+        Entry[] entries = Array.Empty<Entry>();
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            if (IsPlaying) Initialize();
+        }
+        #endregion
+
+        //Static
+        #region
         public const string Name = "Localization";
         public const string Path = Toolbox.Paths.Root + Name + "/";
 
-        [SerializeField]
-        Entry[] entries = Array.Empty<Entry>();
         public static Entry[] Entries => Instance.entries;
+
+        public static Dictionary<string, Entry> Dictionary { get; } = new Dictionary<string, Entry>(StringComparer.OrdinalIgnoreCase);
+
+        public static Entry Selection { get; private set; }
+        public static Entry.TextDictionary Text => Selection.Text;
+
+        public static string Choice
+        {
+            get
+            {
+                return AutoPreferences.Read("Localization/Choice", fallback: Entries[0].Title);
+            }
+            private set
+            {
+                AutoPreferences.Set("Localization/Choice", value);
+            }
+        }
+
+        static void Initialize()
+        {
+            if (Entries.Length == 0)
+                throw new Exception($"No Narrative Localization Entries Set," +
+                    $" Please Set at Least Once in the Project Settings Window");
+
+            for (int i = 0; i < Entries.Length; i++)
+                Entries[i].Load();
+
+            Dictionary.Clear();
+
+            for (int i = 0; i < Entries.Length; i++)
+                Dictionary.Add(Entries[i].Title, Entries[i]);
+
+            Set(Choice);
+        }
+
+        public delegate void SetDelegate(Entry entry);
+        public static event SetDelegate OnSet;
+        public static void Set(string id)
+        {
+            if (Dictionary.TryGetValue(id, out var entry) == false)
+                throw new Exception($"Cannot Set Localization With ID: '{id}' Because No Entry Was Registerd With That ID");
+
+            Choice = id;
+
+            Selection = entry;
+
+            OnSet?.Invoke(Selection);
+        }
+
+        public static string Format(string text, ILocalizationFormat format)
+        {
+            text = Text[text];
+
+            if (format.Phrases.Count == 0) return text;
+
+            using (DisposablePool.StringBuilder.Lease(out var builder))
+            {
+                builder.Append(text);
+
+                foreach (var pair in format.Phrases)
+                    builder.Replace($"{{{pair.Key}}}", Text[pair.Value]);
+
+                return builder.ToString();
+            }
+        }
+        #endregion
 
         [Serializable]
         [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
@@ -87,78 +166,6 @@ namespace MB.LocalizationSystem
                 }
 
                 public TextDictionary() : base(StringComparer.OrdinalIgnoreCase) { }
-            }
-        }
-
-        public static Dictionary<string, Entry> Dictionary { get; } = new Dictionary<string, Entry>(StringComparer.OrdinalIgnoreCase);
-
-        public static Entry Selection { get; private set; }
-        public static Entry.TextDictionary Text => Selection.Text;
-
-        public static string Choice
-        {
-            get
-            {
-                return AutoPreferences.Read("Localization/Choice", fallback: Entries[0].Title);
-            }
-            private set
-            {
-                AutoPreferences.Set("Localization/Choice", value);
-            }
-        }
-
-        protected override void OnLoad()
-        {
-            base.OnLoad();
-
-            if (IsPlaying) Initialize();
-        }
-
-        static void Initialize()
-        {
-            if (Entries.Length == 0)
-                throw new Exception($"No Narrative Localization Entries Set," +
-                    $" Please Set at Least Once in the Project Settings Window");
-
-            for (int i = 0; i < Entries.Length; i++)
-                Entries[i].Load();
-
-            Dictionary.Clear();
-
-            for (int i = 0; i < Entries.Length; i++)
-                Dictionary.Add(Entries[i].Title, Entries[i]);
-
-            Set(Choice);
-        }
-
-        public delegate void SetDelegate(Entry entry);
-        public static event SetDelegate OnSet;
-        public static void Set(string id)
-        {
-            if (Dictionary.TryGetValue(id, out var entry) == false)
-                throw new Exception($"Cannot Set Localization With ID: '{id}' Because No Entry Was Registerd With That ID");
-
-            Choice = id;
-
-            Selection = entry;
-
-            OnSet?.Invoke(Selection);
-        }
-
-        public static string Format(string text, ILocalizationFormat format)
-        {
-            text = Text[text];
-
-            if (format.Phrases.Count == 0) return text;
-
-            using (DisposablePool.StringBuilder.Lease(out var builder))
-            {
-                builder.Append(text);
-
-                foreach (var pair in format.Phrases)
-                    builder.Replace($"{{{pair.Key}}}", Text[pair.Value]);
-
-                return builder.ToString();
             }
         }
     }
